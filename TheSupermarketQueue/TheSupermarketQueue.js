108f9ff88f1d4cee185675dbd3c8f1027a945c00
Joni Rousu <jonirousu@gmail.com>
2020-09-04 11:24:01 +0200

    Complete Supermarket Queue

--- TheSupermarketQueue/TheSupermarketQueue.js
+++ TheSupermarketQueue/TheSupermarketQueue.js
@@ -4,10 +4,34 @@ function queueTime(customers, n) {
       for (customer of customers) {
         time += customer;
       }
+    } else if (n >= customers.length) {
+      time = Math.max(...customers);
     } else {
-      // Distribute the customers to n tills
-      // Keep track of waiting time at till x
-      // Fill empty tills immediately
+      let tills = [];
+      for(let i = 0; i < n; i++) {
+        tills.push(customers.shift());  
+      }
+      while (customers.length > 0 || tills.reduce((pv, cv) => pv + cv, 0) > 0) {
+        let fastestCustomer = Math.min.apply(null, tills.filter(Boolean));
+        time += fastestCustomer;
+        for(let i = 0; i < n; i++) {
+          if (!Number.isNaN(tills[i] - fastestCustomer)) {
+            if (tills[i] - fastestCustomer < 0) {
+              tills[i] = 0;
+            } else {
+              tills[i] -= fastestCustomer; 
+            }
+          }
+        }
+        for (let i = 0; i < n; i++) {
+              if (tills[i] <= 0) {
+                let nextcustomer = customers.shift();
+                if (nextcustomer != undefined) {
+                  tills[i] = nextcustomer;
+                }
+              }
+          }
+      }
     }
     return time;
   }
