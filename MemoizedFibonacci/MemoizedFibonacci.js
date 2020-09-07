cf2b2cd26463eedae9bb84cdbcbe036058fe6c55
Joni Rousu <jonirousu@gmail.com>
2020-09-07 17:55:19 +0200

    Move storage fibTerms into function to keep it private

--- MemoizedFibonacci/MemoizedFibonacci.js
+++ MemoizedFibonacci/MemoizedFibonacci.js
@@ -1,8 +1,11 @@
-let fibTerms = [0,1];
+var fibonacci = (function() {
+    
+    var fibTerms = [0,1];
 
-var fibonacci = function(n) {
-    if (fibTerms[n] == undefined) {
-        fibTerms.push(fibonacci(n-1) + fibonacci(n-2));
-    }
-    return fibTerms[n];
-}
+    return function(n) {
+        if (fibTerms[n] == undefined) {
+            fibTerms.push(fibonacci(n-1) + fibonacci(n-2));
+        }
+        return fibTerms[n];
+    };
+})();
