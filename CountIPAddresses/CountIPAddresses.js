9b85ecf4c254e67e0fa22deab0452bf2a33242eb
Joni Rousu <joni@rousu.de>
2022-11-04 11:44:07 +0100

    Add finished kata Count IP Adresses

--- CountIPAddresses/CountIPAddresses.js
+++ CountIPAddresses/CountIPAddresses.js
@@ -1,3 +1,13 @@
-function ipsBetween(start, end){
-    
-  }
+function ipsBetween(start, end) {
+
+    let IPLength = 4;
+    let startIP = start.split(".");
+    let endIP = end.split(".");
+    let IPsBetween = 0;
+
+    for (let i = 0; i < IPLength; i++) {
+        IPsBetween += (endIP[i]-startIP[i])*(256 ** (IPLength-1-i));
+    }
+
+    return IPsBetween;
+}
