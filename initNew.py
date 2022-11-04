65c255be7358bcf154ea9767a6bb2d07994b5486
Joni Rousu <joni@rousu.de>
2022-11-04 10:59:04 +0100

    Working prototype - now for making it universal

--- initNew.py
+++ initNew.py
@@ -1,10 +1,17 @@
 import requests
+import os
 
 url = 'https://www.codewars.com/api/v1/code-challenges/'
 id = '526989a41034285187000de4' # Count IP-Adresses kata for testing
 resp = requests.get(url=url+id)
 data = resp.json()
-kataName= data['name']
+kataName = data['name']
+kataNameStripped = kataName.replace(" ","")
 description=data['description']
-print(kataName)
-print(description)
+
+os.mkdir(kataNameStripped)
+
+with open(kataNameStripped + '/README.md', 'w') as f:
+    f.write('# '+ kataName + '\n\n' + description)
+
+open(kataNameStripped + '/' + kataNameStripped + '.js', 'x').close()
