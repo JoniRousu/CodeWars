814ae14dce5cf12412b1e12f91221164b4f3da5b
Joni Rousu <joni@rousu.de>
2022-11-04 12:30:25 +0100

    Add horizontal, vertical and diagnonal checks for a winner

--- Tic-Tac-ToeChecker/Tic-Tac-ToeChecker.js
+++ Tic-Tac-ToeChecker/Tic-Tac-ToeChecker.js
@@ -1,3 +1,48 @@
 function isSolved(board) {
-    // TODO: Check if the board is solved!
+
+    // Check horizontally
+
+    for (let i = 0; i < 3; i++) {
+        if (board[i].every( (val, j, arr) => val === arr[0] )) {
+            return board[i][0];
+        }
+    }
+
+    // Check vertically
+    
+    let vert1 = [0,0,0];
+    let vert2 = [0,0,0];
+    let vert3 = [0,0,0];
+
+    for (let i = 0; i < 3; i++) {
+        vert1[i] = board[i][0];
+        vert2[i] = board[i][1];
+        vert2[i] = board[i][2];
+    }
+
+    let vertical = [vert1, vert2, vert3];
+
+    for (let i = 0; i < vertical.length; i++) {
+        if (vertical[i].every( (val, j, arr) => val === arr[0] )) {
+            return vertical[i][0];
+        }
+    }
+
+    // Check diagonally
+
+    let diag1 = [0,0,0];
+    let diag2 = [0,0,0];
+
+    for (let i = 0; i < 3; i++) {
+        diag1[i] = board[i][i];
+        diag2[i] = board[2-i][i];
+    }
+
+    let diagonal = [diag1, diag2];
+
+    for (let i = 0; i < diagonal.length; i++) {
+        if (diagonal[i].every( (val, j, arr) => val === arr[0] )) {
+            return horizontal[i][0];
+        }
+    }
   }
