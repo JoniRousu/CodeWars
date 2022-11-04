0cd6d506aaa8eee0cb67dcad367aeb43625eda62
Joni Rousu <joni@rousu.de>
2022-11-04 11:17:07 +0100

    Make it universal with rudimentary error handling

--- initNew.py
+++ initNew.py
@@ -1,9 +1,20 @@
 import requests
 import os
+import sys
+
+if (len(sys.argv) != 2):
+    print('Missing kata ID!')
+    print('Usage: kata ID as argument.')
+    exit(1)
 
 url = 'https://www.codewars.com/api/v1/code-challenges/'
-id = '526989a41034285187000de4' # Count IP-Adresses kata for testing
+id = sys.argv[1]
 resp = requests.get(url=url+id)
+
+if (resp.status_code != 200):
+    print('The kata ID is invalid or there was an other problem.')
+    exit(1)
+
 data = resp.json()
 kataName = data['name']
 kataNameStripped = kataName.replace(" ","")
@@ -15,3 +26,8 @@ with open(kataNameStripped + '/README.md', 'w') as f:
     f.write('# '+ kataName + '\n\n' + description)
 
 open(kataNameStripped + '/' + kataNameStripped + '.js', 'x').close()
+
+print('Sucessfully initialized kata ' + kataName)
+print('Have fun!')
+
+exit(0)
