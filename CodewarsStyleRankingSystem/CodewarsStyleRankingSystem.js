003892fe6253da2d212131363d34d5371b985765
Joni Rousu <jonirousu@gmail.com>
2022-11-17 13:38:37 +0100

    Add Codewares Style Ranking System kata

--- CodewarsStyleRankingSystem/CodewarsStyleRankingSystem.js
+++ CodewarsStyleRankingSystem/CodewarsStyleRankingSystem.js
@@ -1,2 +1,34 @@
-// TODO: create the User class/object
-// it must support rank, progress and the incProgress(rank) method
+class User {
+    constructor () {
+        this.progress = 0;
+        this.RankValues = [-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8];
+        this.rank = this.RankValues[0];
+    }
+
+    incProgress(rank) {
+        if (this.RankValues.indexOf(rank) === -1) {
+            throw 'Invalid rank';
+        } 
+        if(this.rank < 8) {
+            let d = this.RankValues.indexOf(rank)-this.RankValues.indexOf(this.rank);
+
+            if (d > -2) {
+                switch (d) {
+                    case 0: this.progress += 3; break;
+                    case -1: this.progress += 1; break;
+                    default: this.progress += 10*d*d; break;
+                }
+
+                while (this.progress >= 100) {
+                    this.progress -= 100;
+                    if (this.rank === 7) {
+                        this.progress = 0;
+                        this.rank = 8;
+                    } else {
+                        this.rank = this.RankValues[this.RankValues.indexOf(this.rank)+1];
+                    }
+                }
+            }
+        }
+    }
+}
